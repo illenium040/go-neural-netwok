7ec1f07f30cfc6b03459ad4d37a82a61216d6ff9
illenium <illenium040@mail.ru>
2023-04-16 10:50:50 +0000

    Убрал лишние комментарии

--- src/nnetwork/base.go
+++ src/nnetwork/base.go
@@ -55,7 +55,6 @@ func (n *Network) Propagate(x *mat.Dense) error {
 		return Sigmoid(v)
 	}
 
-	// Данные проходят по всем скрытым слоям
 	for i := 0; i < n.config.HiddenLayresCount; i++ {
 		hiddenLayerInput := new(mat.Dense)
 		if i == 0 {
@@ -85,7 +84,6 @@ func (n *Network) Backpropagate(x, y *mat.Dense) error {
 	networkError := new(mat.Dense)
 	networkError.Sub(y, n.bOut)
 
-	//var nErrors = make([]*mat.Dense, n.config.HiddenLayresCount+1, n.config.HiddenLayresCount+1)
 	var nError = new(mat.Dense)
 	for i := n.config.HiddenLayresCount; i >= 0; i-- {
 		slopeLayer := new(mat.Dense)
@@ -145,9 +143,6 @@ func (n *Network) Train(x, y *mat.Dense) error {
 }
 
 func (n *Network) Predict(x *mat.Dense) (*mat.Dense, error) {
-
-	// Проверяем, представляет ли значение neuralNet
-	// обученную модель.
 	if n.wHidden == nil || n.wOut == nil {
 		return nil, errors.New("the supplied weights are empty")
 	}
@@ -161,26 +156,6 @@ func (n *Network) Predict(x *mat.Dense) (*mat.Dense, error) {
 	applySigmoid := func(_, _ int, v float64) float64 {
 		return Sigmoid(v)
 	}
-	// Данные проходят по всем скрытым слоям
-	//hiddenLayerInput := new(mat.Dense)
-	//for i := 0; i < n.config.HiddenLayresCount; i++ {
-	//	if i == 0 {
-	//		hiddenLayerInput.Mul(x, n.wHidden[i])
-	//	} else {
-	//		hiddenLayerInput.Mul(n.bHidden[i-1], n.wHidden[i])
-	//	}
-	//	hiddenLayerInput.Apply(func(_, col int, v float64) float64 {
-	//		return v + n.bHidden[i].At(0, col)
-	//	}, hiddenLayerInput)
-	//	hiddenLayerInput.Apply(applySigmoid, hiddenLayerInput)
-	//}
-	//
-	//outputLayerInput := new(mat.Dense)
-	//outputLayerInput.Mul(hiddenLayerInput, n.wOut)
-	//outputLayerInput.Apply(func(_, col int, v float64) float64 {
-	//	return v + n.bOut.At(0, col)
-	//}, outputLayerInput)
-	//output.Apply(applySigmoid, outputLayerInput)
 
 	var layers = make([]*mat.Dense, n.config.HiddenLayresCount, n.config.HiddenLayresCount)
 	for i := 0; i < n.config.HiddenLayresCount; i++ {
@@ -232,147 +207,6 @@ func sumAlongAxis(axis int, m *mat.Dense) (*mat.Dense, error) {
 
 	return output, nil
 }
-
-//func (nn *NeuralNet) backpropagate(x, y, wHidden, bHidden, wOut, bOut, output *mat.Dense) error {
-//	for i := 0; i < nn.config.NumEpochs; i++ {
-//
-//		// Завершаем процесс прямого распространения.
-//		hiddenLayerInput := new(mat.Dense)
-//		hiddenLayerInput.Mul(x, wHidden)
-//		addBHidden := func(_, col int, v float64) float64 { return v + bHidden.At(0, col) }
-//		hiddenLayerInput.Apply(addBHidden, hiddenLayerInput)
-//
-//		hiddenLayerActivations := new(mat.Dense)
-//		applySigmoid := func(_, _ int, v float64) float64 { return Sigmoid(v) }
-//		hiddenLayerActivations.Apply(applySigmoid, hiddenLayerInput)
-//
-//		outputLayerInput := new(mat.Dense)
-//		outputLayerInput.Mul(hiddenLayerActivations, wOut)
-//		addBOut := func(_, col int, v float64) float64 { return v + bOut.At(0, col) }
-//		outputLayerInput.Apply(addBOut, outputLayerInput)
-//		output.Apply(applySigmoid, outputLayerInput)
-//
-//		// Завершаем обратное расространение.
-//		networkError := new(mat.Dense)
-//		networkError.Sub(y, output)
-//
-//		slopeOutputLayer := new(mat.Dense)
-//		applySigmoidPrime := func(_, _ int, v float64) float64 { return SigmoidPrime(v) }
-//		slopeOutputLayer.Apply(applySigmoidPrime, output)
-//		slopeHiddenLayer := new(mat.Dense)
-//		slopeHiddenLayer.Apply(applySigmoidPrime, hiddenLayerActivations)
-//
-//		dOutput := new(mat.Dense)
-//		dOutput.MulElem(networkError, slopeOutputLayer)
-//		errorAtHiddenLayer := new(mat.Dense)
-//		errorAtHiddenLayer.Mul(dOutput, wOut.T())
-//
-//		dHiddenLayer := new(mat.Dense)
-//		dHiddenLayer.MulElem(errorAtHiddenLayer, slopeHiddenLayer)
-//
-//		// Регулируем параметры.
-//		wOutAdj := new(mat.Dense)
-//		wOutAdj.Mul(hiddenLayerActivations.T(), dOutput)
-//		wOutAdj.Scale(nn.config.LearningRate, wOutAdj)
-//		wOut.Add(wOut, wOutAdj)
-//
-//		bOutAdj, err := sumAlongAxis(0, dOutput)
-//		if err != nil {
-//			return err
-//		}
-//		bOutAdj.Scale(nn.config.LearningRate, bOutAdj)
-//		bOut.Add(bOut, bOutAdj)
-//
-//		wHiddenAdj := new(mat.Dense)
-//		wHiddenAdj.Mul(x.T(), dHiddenLayer)
-//		wHiddenAdj.Scale(nn.config.LearningRate, wHiddenAdj)
-//		wHidden.Add(wHidden, wHiddenAdj)
-//
-//		bHiddenAdj, err := sumAlongAxis(0, dHiddenLayer)
-//		if err != nil {
-//			return err
-//		}
-//		bHiddenAdj.Scale(nn.config.LearningRate, bHiddenAdj)
-//		bHidden.Add(bHidden, bHiddenAdj)
-//	}
-//
-//	return nil
-//}
-//
-//func (nn *NeuralNet) train(x, y *mat.Dense) error {
-//	randSource := rand.NewSource(time.Now().UnixNano())
-//	randGen := rand.New(randSource)
-//
-//	wHidden := mat.NewDense(nn.config.InputNeurons, nn.config.HiddenNeurons, nil)
-//	bHidden := mat.NewDense(1, nn.config.HiddenNeurons, nil)
-//	wOut := mat.NewDense(nn.config.HiddenNeurons, nn.config.OutputNeurons, nil)
-//	bOut := mat.NewDense(1, nn.config.OutputNeurons, nil)
-//
-//	wHiddenRaw := wHidden.RawMatrix().Data
-//	bHiddenRaw := bHidden.RawMatrix().Data
-//	wOutRaw := wOut.RawMatrix().Data
-//	bOutRaw := bOut.RawMatrix().Data
-//
-//	for _, param := range [][]float64{
-//		wHiddenRaw,
-//		bHiddenRaw,
-//		wOutRaw,
-//		bOutRaw,
-//	} {
-//		for i := range param {
-//			param[i] = randGen.Float64()
-//		}
-//	}
-//
-//	// Определяем выход сети.
-//	output := new(mat.Dense)
-//
-//	// Используем обратное распространение для регулировки весов и смещений.
-//	if err := nn.backpropagate(x, y, wHidden, bHidden, wOut, bOut, output); err != nil {
-//		return err
-//	}
-//
-//	// Определяем обученную сеть.
-//	nn.wHidden = wHidden
-//	nn.bHidden = bHidden
-//	nn.wOut = wOut
-//	nn.bOut = bOut
-//
-//	return nil
-//}
-//
-//func (nn *NeuralNet) predict(x *mat.Dense) (*mat.Dense, error) {
-//
-//	// Проверяем, представляет ли значение neuralNet
-//	// обученную модель.
-//	if nn.wHidden == nil || nn.wOut == nil {
-//		return nil, errors.New("the supplied weights are empty")
-//	}
-//	if nn.bHidden == nil || nn.bOut == nil {
-//		return nil, errors.New("the supplied biases are empty")
-//	}
-//
-//	// Определяем выход сети.
-//	output := new(mat.Dense)
-//
-//	// Завершаем процесс прямого распространения.
-//	hiddenLayerInput := new(mat.Dense)
-//	hiddenLayerInput.Mul(x, nn.wHidden)
-//	addBHidden := func(_, col int, v float64) float64 { return v + nn.bHidden.At(0, col) }
-//	hiddenLayerInput.Apply(addBHidden, hiddenLayerInput)
-//
-//	hiddenLayerActivations := new(mat.Dense)
-//	applySigmoid := func(_, _ int, v float64) float64 { return Sigmoid(v) }
-//	hiddenLayerActivations.Apply(applySigmoid, hiddenLayerInput)
-//
-//	outputLayerInput := new(mat.Dense)
-//	outputLayerInput.Mul(hiddenLayerActivations, nn.wOut)
-//	addBOut := func(_, col int, v float64) float64 { return v + nn.bOut.At(0, col) }
-//	outputLayerInput.Apply(addBOut, outputLayerInput)
-//	output.Apply(applySigmoid, outputLayerInput)
-//
-//	return output, nil
-//}
 
 func Sigmoid(x float64) float64 {
 	return 1.0 / (1.0 + math.Exp(-x))
